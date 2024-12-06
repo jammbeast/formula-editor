c5d456524e7de3c7f73ebdb85dbbaf4e391ce6f3
Jammbeast <jammbeast69@mail.ru>
2024-12-06 20:03:20 +0300

    Test, allright

--- src/app/page.js
+++ src/app/page.js
@@ -4,16 +4,14 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
+        <div className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <div className="mb-2">
+            Test{" "}
+            {/* <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+            </code> */}
             .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+          </div>
+        </div>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
         </div>
